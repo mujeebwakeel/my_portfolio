0bdd684ea2ca9721cd25cf9d65c871cdafed81e4
mujeebwakeel <wakeelmujeeb@yahoo.com>
2020-09-06 07:59:46 +0100

    text-middle added to thumbnail

---
+++ public/script/navlist.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+	$( function() {
+    $(".navlist").click(function() {
+        $(".navlist").removeClass("selected");
+        $(this).addClass("selected");
+    });
+    });
+});
