1560cbf46808ab22bd30b570e2bc771ec32c8684
mujeebwakeel <wakeelmujeeb@yahoo.com>
2020-09-07 17:00:35 +0100

    Portfolio re-branded

--- public/script/navlist.js
+++ public/script/navlist.js
@@ -1,8 +1,40 @@
 $( document ).ready(function() {
 	$( function() {
-    $(".navlist").click(function() {
-        $(".navlist").removeClass("selected");
-        $(this).addClass("selected");
-    });
+        
+        window.onscroll = function() {myFunction()};
+
+        var home = document.getElementById("home");
+        var portfolio = document.getElementById("portfolio");
+        var about = document.getElementById("about");
+        var contact = document.getElementById("contact");
+        var resume = document.getElementById("resume");
+
+        var homeSticky = home.offsetTop;
+        var portfolioSticky = portfolio.offsetTop;
+        var aboutSticky = about.offsetTop;
+        var contactSticky = contact.offsetTop;
+        var resumeSticky = resume.offsetTop;
+
+        function myFunction() {
+        if (window.pageYOffset < aboutSticky) {
+            $(".navlist").removeClass("selected");
+            $(".navlist:eq(0)").addClass("selected");
+        } else if(window.pageYOffset > homeSticky && window.pageYOffset < portfolioSticky){
+            $(".navlist").removeClass("selected");
+            $(".navlist:eq(1)").addClass("selected");
+        } else if(window.pageYOffset > aboutSticky && window.pageYOffset < resumeSticky) {
+            $(".navlist").removeClass("selected");
+            $(".navlist:eq(2)").addClass("selected");
+        } else if(window.pageYOffset > portfolioSticky && window.pageYOffset < contactSticky) {
+            $(".navlist").removeClass("selected");
+            $(".navlist:eq(3)").addClass("selected");
+        } else if(window.pageYOffset >= contactSticky) {
+            $(".navlist").removeClass("selected");
+            $(".navlist:eq(4)").addClass("selected");
+        }
+        }
+
     });
 });
+
+
--- public/script/slideshow.js
+++ public/script/slideshow.js
@@ -18,3 +18,31 @@ function showSlides() {
   setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 
+
+// manual slide for resume
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+function plusSlides(n) {
+  showSlides1(slideIndex1 += n);
+}
+
+function currentSlide(n) {
+  showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
+  var i;
+  var slides = document.getElementsByClassName("resume-slide");
+  var dots = document.getElementsByClassName("dot1");
+  if (n > slides.length) {slideIndex1 = 1}    
+  if (n < 1) {slideIndex1 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active1", "");
+  }
+  slides[slideIndex1-1].style.display = "block";  
+  dots[slideIndex1-1].className += " active1";
+}
